64a3e4c5dda04ca85614e7614310fe12b1e17861
hemalathapuvvula <hemalathapuvvula15@gmail.com>
2026-08-03 20:18:27 +0530

    Add files via upload

---
+++ DataTypeIdentify2.py
@@ -0,0 +1,11 @@
+#program using tuple, list, and dictionary
+fruits=["apple", "banana", "cherry"] #list
+colors=("red", "blue", "yellow") #tuple
+student={
+    "name": "hemalatha",
+    "age":17,
+    "branch": "CME"
+    } #dictinary
+print("list:", fruits)
+print("tuple:", colors)
+print("dictionary:", student)
