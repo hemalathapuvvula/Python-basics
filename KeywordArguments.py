282c76d8ead1fa0e1535a60aaa90a2efcf3cfba9
Hemalatha Puvvula <hemalathapuvvula15@gmail.com>
2026-08-16 17:34:22 +0530

    Python basic program
    Keyword arguments (also called named arguments) allow you to pass values to a function by explicitly stating the parameter name followed by an equals sign (name=value). 
    This program explains keyword arguments

---
+++ KeywordArguments.py
@@ -0,0 +1,8 @@
+#program using function with keyword arguments in python
+#creating a function using def keyword
+def greet(first_name, last_name):
+    print(f"Hello, {first_name} {last_name}!")
+
+# Ordering does not matter with keyword arguments
+greet(last_name="Hemalatha", first_name="Puvvula") 
+
