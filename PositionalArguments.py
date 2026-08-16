9b53a41854cea5552128fcb4c5e09f293d700613
Hemalatha Puvvula <hemalathapuvvula15@gmail.com>
2026-08-16 17:25:16 +0530

    Basic Python Program
    Positional arguments in Python are values passed to a function based strictly on the order in which its parameters are defined. The first value goes to the first parameter, the second value goes to the second parameter. This program explains positional arguments

---
+++ PositionalArguments.py
@@ -0,0 +1,7 @@
+#program using function with positional arguments in python
+#creating a function using def keyword
+def add(a,b):
+ return a+b
+#calling a function with positional arguments
+result=add(3,5)
+print("result=", result)
